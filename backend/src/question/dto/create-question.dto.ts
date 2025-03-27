182d1c33d11c19336a918274df0f39349e30a6cb
Gadza03 <mgadza@pmfst.hr>
2025-03-27 23:37:36 +0100

    Swagger setup

--- backend/src/question/dto/create-question.dto.ts
+++ backend/src/question/dto/create-question.dto.ts
@@ -1,1 +1,12 @@
-export class CreateQuestionDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateQuestionDto {
+  @ApiProperty({ description: 'Question title' })
+  title: string;
+
+  @ApiProperty({ description: 'Question description' })
+  description: string;
+
+  @ApiProperty({ description: 'Question ctegory' })
+  category: string;
+}
